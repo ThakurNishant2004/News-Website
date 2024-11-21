8f4d26a1d68f3a713f4e023a2ca8f7f78e8cf5f8
ThakurNishant2004 <piyushchauhan200417@gmail.com>
2024-11-22 00:54:37 +0530

    add styling, add card for news & add url function to redirect into the news application webpage

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,23 +2,27 @@ import React from 'react'
 
 const Card = ({data}) => {
   console.log(data);
-  
+  // const readMore =(url)=>{
+  //   window.open(url)
+  // }
   return (
     <div className='cardContainer'>
       {data.map((curData,index)=>{
         return (
           <div className='card'>
                <img src={curData.urlToImage}/>
-               <div className='cardDescription'>
-               <a>{curData.title}</a>
-               <p>{curData.desc}</p>
-               <button>Read more...</button>
+               <div className='content'>
+               <a className='title' onClick={()=>{window.open(curData.url)}}>{curData.title}</a>
+               <p>{curData.description}</p>
+               <button onClick={()=>{window.open(curData.url)}}>Read more...</button>
                </div>
           </div>
         )
       })}
     </div>
   )
+  console.log(Card);
+  
 }
 
 export default Card
